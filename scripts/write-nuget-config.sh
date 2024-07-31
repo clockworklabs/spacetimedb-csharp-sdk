dd3d89aae27e0e7f904ed3b5a09912bfc2c665e5
Zeke Foppa <bfops@users.noreply.github.com>
2024-07-31 07:12:40 -0700

    comments

--- scripts/write-nuget-config.sh
+++ scripts/write-nuget-config.sh
@@ -7,9 +7,7 @@ cd ..
 
 # Write out the nuget config file to `nuget.config`. This causes the spacetimedb-csharp-sdk repository
 # to be aware of the local versions of the `bindings-csharp` packages in SpacetimeDB, and use them if
-# available. Otherwise, `spacetimedb-csharp-sdk` will use the NuGet versions of the packages.
-# This means that (if version numbers match) we will test the local versions of the C# packages, even
-# if they're not pushed to NuGet.
+# available.
 # See https://learn.microsoft.com/en-us/nuget/reference/nuget-config-file for more info on the config file,
 # and https://tldp.org/LDP/abs/html/here-docs.html for more info on this bash feature.
 cat >nuget.config <<EOF
